894c97427698c619baf4b6dca13f9fa1b1feb300
noemimedeiros <noemimedeiros.acc@gmail.com>
2024-10-07 17:29:23 -0300

    Sidenav com subitens

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -18,7 +18,7 @@ class PostListView(LoginRequiredMixin, MyListViewIxoyeConnect):
 
     def get_queryset(self):
         qs = super().get_queryset()
-        return qs.filter(categoria__nome='')
+        return qs.filter(categoria__nome=self.kwargs['tipo'])
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
