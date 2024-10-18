10f59e7e7d9fa6708a675b47b2561279832fe560
noemimedeiros <noemimedeiros.acc@gmail.com>
2024-10-18 10:15:35 -0300

    Correções de arquivos estáticos

--- apps/core/static/serviceworker.js
+++ apps/core/static/serviceworker.js
@@ -1,10 +1,36 @@
-self.addEventListener('push', function(event) {
-    const data = event.data.json();
-    const options = {
-        body: data.body,
-        icon: data.icon,
-    };
-    event.waitUntil(
-        self.registration.showNotification(data.title, options)
-    );
+var staticCacheName = 'ixoye-connect-v1';
+
+var assetsToCache = [
+  '/layouts/base',
+  '/static/img/bottom-desktop-2.svg',
+  '/static/img/bottom-desktop.svg',
+  '/static/img/top-nav-mobile-2.svg',
+  '/static/img/top-nav-mobile.svg',
+
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      console.log('Caching static assets');
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  var requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin === location.origin) {
+    if (requestUrl.pathname === '/') {
+      event.respondWith(caches.match('/layouts/base'));
+      return;
+    }
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
--- ixoyeconnect/settings.py
+++ ixoyeconnect/settings.py
@@ -259,33 +259,3 @@ WEBPUSH_SETTINGS = {
    "VAPID_PRIVATE_KEY": env("VAPID_PRIVATE_KEY", default=""),
    "VAPID_ADMIN_EMAIL": env("VAPID_ADMIN_EMAIL", default="")
 }
-
-# -----------------------------------------------------------------------------
-# PWA WEBPUSH
-# -----------------------------------------------------------------------------
-PWA_APP_NAME = 'IxoyeConnect'
-PWA_APP_DESCRIPTION = "IxoyeConnect"
-PWA_APP_THEME_COLOR = '#3DBD90'
-PWA_APP_BACKGROUND_COLOR = '#ffffff'
-PWA_APP_DISPLAY = 'standalone'
-PWA_APP_SCOPE = '/'
-PWA_APP_ORIENTATION = 'any'
-PWA_APP_START_URL = '/'
-PWA_APP_ICONS = [
-    {
-        'src': '/static/public/img/favicon/android-chrome-512x512.png',
-        'sizes': '512x512'
-    },
-    {
-        'src': '/static/public/img/favicon/android-chrome-192x192.png',
-        'sizes': '192x192'
-    }
-]
-PWA_APP_SPLASH_SCREEN = [
-    {
-        'src': '/static/public/img/favicon/splash_screen.png',
-        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
-    }
-]
-PWA_APP_DIR = 'ltr'
-PWA_APP_LANG = 'pt-BR'
